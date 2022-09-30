b2a9ddef923028b0a81469cb9115a6743e77bcb6
Alan <32980798+alanbarra@users.noreply.github.com>
2022-09-29 21:28:10 -0300

    Update index.js

--- src/Sobre/index.js
+++ src/Sobre/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { View, Text, Button } from 'react-native';
 
 export default function SobreScreen() {
     const navigation = useNavigation();
+    const route = useRoute();
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Sobre Screen</Text>
             <Button title="Voltar para Home" onPress={() => navigation.navigate('Home')} />
+            <Text>{route.params.nome}</Text>
         </View>
     );
 }
